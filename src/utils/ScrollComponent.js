43374795540060288f9664d3980efa768171fe17
Mang-developer <chiwon618@gmail.com>
2020-09-21 22:33:37 +0900

    Feat: Made scroll Event to print scrollTop

--- src/utils/ScrollComponent.js
+++ src/utils/ScrollComponent.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const ScrollComponent = () => {
+  const onScroll = (e) => {
+    if (!e.srcElement) return;
+    console.log(e.srcElement.scrollingElement.scrollTop);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+  }, []);
+
   return <div>스크롤 컴포넌트</div>;
 };
 
